c2732fcd46f4b8950378c839ec4396f720b07010
Spellbringer <Ineedmoreweekends@gmail.com>
2023-01-02 18:06:40 +0300

    Проверка номеров карт при помощи формулы Луна

---
+++ CardValidator.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//Проверка номеров карт при помощи формулы Луна
+public class CardValidator {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String cardNum = reader.readLine();
+        int[] reverseCardNum = new int[cardNum.length()];
+        int sum = 0;
+        for (int i = reverseCardNum.length, j = 0; i > 0; i--, j++) {
+            reverseCardNum[j] = Integer.parseInt(String.valueOf(cardNum.charAt(i - 1)));
+        }
+        for (int i = 0; i < reverseCardNum.length; i++) {
+            if (i % 2 != 0) {
+                reverseCardNum[i] *= 2;
+            }
+        }
+        for (int i = 0; i < reverseCardNum.length; i++) {
+            if (reverseCardNum[i] > 9) {
+                reverseCardNum[i] -= 9;
+
+            }
+        }
+        for (int j : reverseCardNum) {
+            sum += j;
+        }
+        if (sum % 10 == 0) {
+            System.out.println("valid");
+        }
+        else {
+            System.out.println("not valid");
+        }
+    }
+}
